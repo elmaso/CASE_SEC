4c209cbef7178cc71e9a4cdb485d2ed02683d63f
Alejandro Alberto Moreno Zatarain <alalmorenoza@ittepic.edu.mx>
2019-11-21 12:28:39 -0800

    Validacion con Changes notifier

--- lib/screens/prestamos.dart
+++ lib/screens/prestamos.dart
@@ -1,114 +1,41 @@
-import 'package:des_case_app/services/models.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:des_case_app/constants/constants.dart';
 
-import '../services/services.dart';
-import '../shared/shared.dart';
 
-class PrestamosScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Global.empleadoRef.getData(),
-      builder: (BuildContext context, AsyncSnapshot snap) {
-        if (snap.hasData) {
-          List<Empleado> topics = snap.data;
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.deepPurple,
-              title: Text('Prestamos'),
-              actions: [
-                IconButton(
-                  icon: Icon(FontAwesomeIcons.userCircle,
-                      color: Colors.pink[200]),
-                  onPressed: () => Navigator.pushNamed(context, '/Profile'),
-                )
-              ],
-            ),
-            body: GridView.count(
-              primary: false,
-              padding: const EdgeInsets.all(20.0),
-              crossAxisSpacing: 10.0,
-              crossAxisCount: 2,
-              children: topics.map((topic) => TopicItem(topic: topic)).toList(),
-            ),
-          );
-        } else {
-          return LoadingScreen();
-        }
-      },
-    );
-  }
+class PrestamosScreen extends StatefulWidget {
+  createState() => PrestamosScreenState();
 }
 
-class TopicItem extends StatelessWidget {
-  final Empleado topic;
-  const TopicItem({Key key, this.topic}) : super(key: key);
-
+class PrestamosScreenState extends State<PrestamosScreen> {
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Hero(
-        child: Card(
-          clipBehavior: Clip.antiAlias,
-          child: InkWell(
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (BuildContext context) => TopicScreen(topic: topic),
-                ),
-              );
-            },
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 10, right: 10),
-                        child: Text(
-                          topic.nombres,
-                          style: TextStyle(
-                              height: 1.5, fontWeight: FontWeight.bold),
-                          overflow: TextOverflow.fade,
-                          softWrap: false,
-                        ),
-                      ),
-                    ),
-                    // Text(topic.description)
-                  ],
-                ),
-                // )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
 
-class TopicScreen extends StatelessWidget {
-  final Empleado topic;
-
-  TopicScreen({this.topic});
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-      ),
-      body: ListView(children: [
-        Text(
-          topic.nombres,
-          style:
-              TextStyle(height: 2, fontSize: 20, fontWeight: FontWeight.bold),
+      body: Container(
+        padding: EdgeInsets.all(30),
+        decoration: BoxDecoration(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: <Widget>[
+                Image.asset(seciniciodos),
+                SizedBox(
+                  height: 50.0,
+                  width: 50.0,
+                ),
+              ],
+            ),
+            SizedBox(height: 3.0),
+            Text(
+              'Prestamis',
+              style: Theme.of(context).textTheme.headline,
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
-      ]),
+      ),
     );
   }
 }
--- lib/screens/validacion.dart
+++ lib/screens/validacion.dart
@@ -1,14 +1,14 @@
 import 'package:des_case_app/screens/login.dart';
-import 'package:des_case_app/services/db.dart';
 import 'package:flutter/material.dart';
 import 'package:des_case_app/constants//images.dart';
+import 'package:des_case_app/services/db.dart';
+import 'package:provider/provider.dart';
 import 'package:des_case_app/services/models.dart';
 import 'package:des_case_app/services/globals.dart';
-import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 
-
-// Info Compartida
 class ValidacionUsuarioState with ChangeNotifier {
   int _validado = 0;
   String _idemp = '25140306';
@@ -17,117 +17,130 @@ class ValidacionUsuarioState with ChangeNotifier {
 
   final PageController controller = PageController();
 
-  get validado = _validado;
+  get validado => _validado;
 
-  set validado(int newValue){
+  get idemp => _idemp;
+
+  get ss => _ss;
+
+  get intentos => _intentos;
+
+  set validado(int newValue) {
     _validado = newValue;
     notifyListeners();
   }
-  void nextPage() async {
-    await controller.nextPage(duration: Duration(milliseconds: 500),
-        curve: Curves.easeInOut,
-    );
+
+  set idemp(String newValue) {
+    _idemp = newValue;
+    notifyListeners();
+  }
+
+  set ss(String newValue) {
+    _ss = newValue;
+    notifyListeners();
+  }
+
+  set intentos(int newValue) {
+    _intentos = newValue;
+    notifyListeners();
   }
 
 
-
-
-
+  void nextPage() async {
+    await controller.nextPage(
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+}
 
 class ValidacionUsuario extends StatelessWidget {
-  //ValidacionUsuario({this.})
-
+  ValidacionUsuario({this.empleadoId});
+  final String empleadoId;
+  final numempleado = TextEditingController();
   @override
-  Widget build(BuildContext context){
+
+  Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       builder: (_) => ValidacionUsuarioState(),
       child: FutureBuilder(
-        future: Document<Empleado>(path: 'empleados/25140356').getData(),
+        future: Document<Empleado>(path: 'Empleados/$empleadoId').getData(),
         builder: (BuildContext context, AsyncSnapshot snap) {
-  var state = Provider.of<ValidacionUsuarioState>(context);
+          var state = Provider.of<ValidacionUsuarioState>(context);
 
-  if (!snap.hasData || snap.hasError){
-  return null; //faloo
-  }else {
-  return null;
-  }
-  }
-  }
-
-}
-
-//class _ValidacionUsuarioState extends State<ValidacionUsuario> {
-//  final numempleado = TextEditingController();
-//  @override
-//  Widget build(BuildContext context) {
-//    return FutureBuilder(
-//      future: Global.empleadoRef.getData(),
-//      builder: (BuildContext context, AsyncSnapshot snap) {
-//    if (snap.hasData){
-//    List<Empleado> empleados = snap.data;
-//    return Scaffold(
-//    body:
-//    SafeArea(
-//    child: ListView(
-//    padding: EdgeInsets.symmetric(horizontal: 24.0),
-//    children: <Widget>[
-//    SizedBox(height: 10.0),
-//    Column(
-//    children: <Widget>[
-//    Image.asset(seciniciodos),
-//    SizedBox(height: 10.0),
-//    Text('Validación Socios Activos',
-//    style: TextStyle(fontWeight: FontWeight.bold),),
-//    ],
-//    ),
-//    SizedBox(height: 10.0),
-//    //color
-//    TextField(
-//    controller: numempleado,
-//    cursorColor: Colors.teal,
-//    cursorRadius: Radius.circular(14.0),
-//    cursorWidth: 16.0,
-//    decoration: InputDecoration(
-//    labelText: '# de Empleado',
-//    border: OutlineInputBorder()
-//    ),
-//    ),
-//    SizedBox(height: 28.0),
-//    TextField(
-//    cursorColor: Colors.teal,
-//    cursorRadius: Radius.circular(14.0),
-//    cursorWidth: 16.0,
-//    decoration: InputDecoration(
-//    labelText: '# de SS',
-//    border: OutlineInputBorder()
-//    ),
-//    ),
-//    SizedBox(height: 20.0),
-//    FlatButton(
-//    textColor: Colors.white,
-//    color: Colors.teal,
-//    child: Text('VALIDAR'),
-//    onPressed: () {
-//    if numempleado = empleados.map((empleado) => Empleado(empleado: id)).toList(),
-//    else
-//
-//    }
-//    ),
-//    Column(
-//    children: <Widget>[
-//    SizedBox(height: 30.0),
-//    Text('CAET',
-//    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
-//    ),
-//    ],
-//    ),
-//    ],
-//    ),
-//    ),
-//    );
-//    } else {
-//    return LoginScreen();
-//    }
-//    },
-//    }
- // }
+          if (!snap.hasData || snap.hasError) {
+            return LoginScreen();
+          } else {
+            Empleado empleado = snap.data;
+            return SafeArea(
+              child: ListView(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                children: <Widget>[
+                  SizedBox(height: 10.0),
+                  Column(
+                    children: <Widget>[
+                      Image.asset(seciniciodos),
+                      SizedBox(height: 10.0),
+                      Text('Validación Socios Activos',
+                        style: TextStyle(fontWeight: FontWeight.bold),),
+                    ],
+                  ),
+                  SizedBox(height: 10.0),
+                  //color
+                  TextField(
+                    controller: numempleado,
+                    cursorColor: Colors.teal,
+                    cursorRadius: Radius.circular(14.0),
+                    cursorWidth: 16.0,
+                    decoration: InputDecoration(
+                        labelText: '# de Empleado',
+                        border: OutlineInputBorder()
+                    ),
+                  ),
+                  SizedBox(height: 28.0),
+                  PageView.builder(
+                    controller: state.controller,
+                  ),
+                  TextField(
+                    controller: numempleado,
+                    cursorColor: Colors.teal,
+                    cursorRadius: Radius.circular(14.0),
+                    cursorWidth: 16.0,
+                    decoration: InputDecoration(
+                        labelText: '# de Empleado',
+                        border: OutlineInputBorder()
+                    ),
+                  ),
+                  SizedBox(height: 28.0),
+                  TextField(
+                    cursorColor: Colors.teal,
+                    cursorRadius: Radius.circular(14.0),
+                    cursorWidth: 16.0,
+                    decoration: InputDecoration(
+                        labelText: '# de SS',
+                        border: OutlineInputBorder()
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  FlatButton(
+                      textColor: Colors.white,
+                      color: Colors.teal,
+                      child: Text('VALIDAR'),
+                      onPressed: () {
+                        if (numempleado != idemp) {
+                          Navigator.pushReplacementNamed(context, '/prestamos');
+                        } else {
+                          Text('Primer intento fallido');
+                        }
+                      }
+                  ),
+                ],
+              ),
+            );
+          }
+        }
+          ),
+    );
+          }
+          var idemp= db.collection('Empleados').doc('id');
+        }
